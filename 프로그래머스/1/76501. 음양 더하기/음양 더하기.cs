08945d078292af4e8a2d9b6d488c8a8cdb70b535
종 욱 <96651722+levell1@users.noreply.github.com>
2023-11-27 09:15:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int[] absolutes, bool[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i < signs.Length; i++)
+        {
+            if(signs[i]==true)
+            {
+                answer+= absolutes[i];
+            }else
+            {
+                answer-= absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}
