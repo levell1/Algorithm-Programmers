625b9a001406a566e5b6196fbe85fc07a059ae5f
종 욱 <96651722+levell1@users.noreply.github.com>
2023-12-01 09:46:17 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 1.93 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        char[] carr= s.ToCharArray();
+        
+        Array.Sort(carr);
+        Array.Reverse(carr);
+        
+        answer = new String(carr);
+        return answer;
+    }
+}
