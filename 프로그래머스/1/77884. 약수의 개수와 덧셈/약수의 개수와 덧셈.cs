5c12ee0bb13425f6f32ffc4154f38b6a0734ddca
종 욱 <96651722+levell1@users.noreply.github.com>
2023-12-01 09:32:24 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int count =0;
+        for(int i = left ; i<=right;i++)
+        {
+            count =0;
+            for(int j =1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+
+                }
+            }
+            if(count%2==0)
+            {
+                answer+= i;
+            }else if(count % 2 !=0)
+            {
+                answer= answer- i ;
+            }
+        }
+        return answer;
+    }
+}
