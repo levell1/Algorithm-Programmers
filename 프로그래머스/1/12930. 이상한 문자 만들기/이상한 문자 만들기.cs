8cba5f26304769d5695869b6a96fc8ec6e9c5c1b
종 욱 <96651722+levell1@users.noreply.github.com>
2023-12-07 09:55:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.81 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public string solution(string s) {
         string answer = "";
+        s= s.ToUpper();
         string[] words = s.Split(' ');
         for(int i =0; i<words.Length;i++)
         {
-            words[i]=words[i].ToUpper();
             char[] str = words[i].ToCharArray();
             for(int j=1; j<words[i].Length; j++)
             {
