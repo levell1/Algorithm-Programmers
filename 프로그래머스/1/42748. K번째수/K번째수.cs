32d6c11aa4985598b7c732bb4454a40531cc6a8a
종 욱 <96651722+levell1@users.noreply.github.com>
2023-12-25 13:29:55 +0900

    [level 1] Title: K번째수, Time: 1.40 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        int[] answer = new int[commands.GetLength(0)] ;
+        int[] a;
+        for(int i = 0; i < commands.GetLength(0) ;i++)
+        {
+            int q = commands[i,0]-1;
+            int p = commands[i,1]-1;
+            a= new int[p-q+1];
+            if(p!=q)
+            {
+                for(int j = q; j<=p; j++)
+                {
+                    a[j-q]= array[j];
+                }
+            }else
+            {
+                a[0]=array[commands[i,1]-1];
+            }
+            Array.Sort(a);
+            int z = commands[i,2]-1;
+            answer[i]=a[z];
+                
+
+        }
+        
+        return answer;
+    }
+}
