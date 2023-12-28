0d2b43b6aeb2ff8b1c500459311704d0f395ecb9
levell1 <96651722+levell1@users.noreply.github.com>
2023-12-28 09:15:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.47 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -2,23 +2,18 @@ using System;
 
 public class Solution {
     public string solution(int[] food) {
-        string answer = "";
-        string f="0";
+        string answer = "0";
         int a = 0;
         for(int i= food.Length-1;i>0;i--)
         {
             a = food[i]/2;
-            if(a>0)
-            {
                 for(int j=0;j<a;j++)
                 {
-                    f = i.ToString() + f;
-                    f += i.ToString();
-                }
-            }
-            
+                    answer = i.ToString() + answer;
+                    answer += i.ToString();
+                }           
         }
-        answer=f;
+
         return answer;
     }
 }
