b8a412830296bf40a9b4d3acd8108ae0a22caaaf
levell1 <96651722+levell1@users.noreply.github.com>
2024-01-09 10:32:12 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.86 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -5,18 +5,18 @@ public class Solution {
         int[] answer = new int[score.Length];
         List<int> list= new List<int>();
         
-        for(int i =0 ; i<score.Length ; i++){
-                list.Add(score[i]);
-                list.Sort();
+        for(int i =0 ; i<score.Length ; i++)
+        {
+            list.Add(score[i]);
+            list.Sort();
+            
             if(i>=k)
             {
                 list.RemoveAt(0);
             }
-                answer[i] = list[0];
-                
+            answer[i] = list[0];
         }
         
-
         return answer;
     }
 }
