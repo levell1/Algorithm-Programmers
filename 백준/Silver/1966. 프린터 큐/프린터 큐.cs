030a243e0915e6b338b1bca7034df40a340b8a41
levell1 <96651722+levell1@users.noreply.github.com>
2024-05-23 15:49:25 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 6520 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorism1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            Queue<(int, bool)> queue = new Queue<(int, bool)>();
+
+            int count = 0;
+            int max = 9;
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                string[] a = Console.ReadLine().Split(' ');
+                string[] b = Console.ReadLine().Split(' ');
+                for (int j = 0; j < int.Parse(a[0]); j++)
+                {
+                    int num = int.Parse(b[j]);
+                    if (j == int.Parse(a[1]))
+                    {
+                        queue.Enqueue((num,true));
+                    }
+                    else
+                    {
+                        queue.Enqueue((num,false));
+                    }
+                }
+
+                while (true)
+                {
+                    if (queue.Peek() == (max, true))
+                    {
+                        count++;
+                        queue.Clear();
+                        break;
+                    }
+                    else if (queue.Peek() == (max, false))
+                    {
+                        queue.Dequeue();
+                        count++;
+                    } else if (!(queue.Contains((max, true)) || queue.Contains((max, false))))
+                    {
+                        max--;
+                    }
+                    else
+                    {
+                        queue.Enqueue(queue.Dequeue());
+                    }
+                    
+                }
+
+                sb.Append(count + "\n");
+                count = 0;
+                max = 9;
+            }
+            Console.Write(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
