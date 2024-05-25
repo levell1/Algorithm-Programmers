8a5c8d6a4ea394a0b2b88c3f8f2e913656f6cbce
levell1 <96651722+levell1@users.noreply.github.com>
2024-05-25 14:19:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        int inputNum = int.Parse(Console.ReadLine());
+
+        int[] dp = new int[1001];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= inputNum; i++)
+        {
+            dp[i] = dp[i - 2] + dp[i - 1];
+            dp[i] %= 10007;
+        }
+        Console.WriteLine(dp[inputNum]);
+        Console.ReadLine();
+    }
+}
