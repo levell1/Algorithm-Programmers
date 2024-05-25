1ad16b4060bdf863d6a58918a240cd5bc7958680
levell1 <96651722+levell1@users.noreply.github.com>
2024-05-25 13:56:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 5616 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
@@ -14,8 +14,7 @@ class Program
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 4;
-        for (int i = 4; i < dp.Length; i++)
+        for (int i = 3; i < dp.Length; i++)
         {
             dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
