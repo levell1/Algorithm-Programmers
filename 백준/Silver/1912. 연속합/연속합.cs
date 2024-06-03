0fcaf593ab3bd5eaddeeceef100c398fa8bb59a8
levell1 <96651722+levell1@users.noreply.github.com>
2024-06-03 17:18:04 +0900

    [Silver II] Title: 연속합, Time: 88 ms, Memory: 12728 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] dp = new int[inputNum];
+            int[] sequence = new int[inputNum];
+            
+            for (int i = 0; i < inputNum; i++)
+            {
+                sequence[i] = int.Parse(numbers[i]);
+                dp[i] = sequence[i];
+            }
+
+            int max = dp.Max();
+            for (int i = 1; i < inputNum; i++)
+            {
+                dp[i] = Math.Max(sequence[i], sequence[i] + dp[i-1]);
+                max = Math.Max(max, dp[i]);
+
+            }
+            
+
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
