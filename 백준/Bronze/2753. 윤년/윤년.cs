77740fb5480fdd60bf4c8e86faada2f4d3434103
levell1 <96651722+levell1@users.noreply.github.com>
2024-08-29 15:09:26 +0900

    [Bronze V] Title: 윤년, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cs
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -12,21 +12,13 @@ namespace Algorithm
             int num = Convert.ToInt32(input);
             Console.Clear();
 
-            if (num%400==0)
+            if ((num % 4 == 0 && num % 100 != 0) || (num % 400 == 0))
             {
-                Console.WriteLine(1);
-            }
-            else if(num%100==0) 
-            {
-                Console.WriteLine (0);
-            }
-            else if (num%4 ==0)
-            {
-                Console.WriteLine(1);
+                Console.WriteLine(1); 
             }
             else
             {
-                Console.WriteLine (0);
+                Console.WriteLine(0); 
             }
 
 
