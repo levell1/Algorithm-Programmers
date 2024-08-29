7ba2b15cfa1569f711c50481568759ee085eb700
levell1 <96651722+levell1@users.noreply.github.com>
2024-08-29 13:48:44 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,8 @@
+ string[] input = Console.ReadLine().Split(' ');
+
+ int a = Convert.ToInt32(input[0]);
+ int b = Convert.ToInt32(input[1]);
+
+ Console.Clear();
+ Console.WriteLine(a + b);
+ Console.ReadLine();
