cfba67448099c4c00cf54bb11464cfad6ce8b4c7
levell1 <96651722+levell1@users.noreply.github.com>
2024-08-29 13:49:47 +0900

    [Bronze V] Title: A-B, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(input[0]);
+            int b = Convert.ToInt32(input[1]);
+
+            Console.Clear();
+            Console.WriteLine(a - b);
+            Console.ReadLine();
+        }
+    }
+}
