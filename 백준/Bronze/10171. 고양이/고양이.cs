96b4348ed4338f99c1efac769500ed5f87bfe2f2
levell1 <96651722+levell1@users.noreply.github.com>
2024-08-30 14:20:19 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+            Console.ReadLine();
+        }
+    }
+}
