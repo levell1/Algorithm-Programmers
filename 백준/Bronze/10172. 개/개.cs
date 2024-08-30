dc268b1062a7b4fa0c08ae0e7712963dad218827
levell1 <96651722+levell1@users.noreply.github.com>
2024-08-30 14:23:21 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }
+    }
+}
