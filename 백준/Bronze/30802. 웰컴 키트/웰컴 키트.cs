e9b0a5d31693afeb2e268ead163af075dd9bc993
levell1 <96651722+levell1@users.noreply.github.com>
2024-09-10 15:54:16 +0900

    [Bronze III] Title: 웰컴 키트, Time: 52 ms, Memory: 6212 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        string people = Console.ReadLine();
+        int peoplei = Convert.ToInt32(people);
+
+        string size= Console.ReadLine();
+        string[] sizearr = size.Split(' ');
+        int[] sizearrint = new int[sizearr.Length];
+
+        string nums = Console.ReadLine();
+        string[] numsarr = nums.Split(' ');
+        int t = Convert.ToInt32(numsarr[0]);
+        int p = Convert.ToInt32(numsarr[1]);
+
+        int answert=0;
+        int answerp;
+
+
+        for (int i = 0; i < sizearr.Length; i++)
+        {
+            sizearrint[i] = Convert.ToInt32(sizearr[i]);
+            answert += (sizearrint[i] / t)+1;
+            if (sizearrint[i] % t==0)
+            {
+                answert--;
+            }
+        }
+        answerp = peoplei / p;
+
+        Console.WriteLine(answert);
+        Console.Write($"{peoplei / p} {peoplei % p}");
+
+        Console.ReadLine();
+    }
+}
