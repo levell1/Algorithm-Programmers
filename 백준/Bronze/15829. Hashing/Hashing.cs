1e0e2901d997a6dc35adf60c5e28f155b947d940
levell1 <96651722+levell1@users.noreply.github.com>
2024-09-13 22:05:04 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 5504 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cs
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -4,25 +4,22 @@ class Program
 {
     static void Main()
     {
-        string input = Console.ReadLine();
-        int L = int.Parse(input);
+        int L = int.Parse(Console.ReadLine()); // 문자열의 길이 입력
+        string input = Console.ReadLine();     // 문자열 입력
 
-        int r = 31;
-        int M = 1234567891;
-        int hash;
-        int pow = 0;
-        double result = 0;
+        long hashValue = 0;
+        long r = 31; // 고정된 r 값
+        long M = 1234567891; // 고정된 M 값
+        long pow = 1; // r의 거듭제곱 값을 저장하는 변수
 
-        string inputword = Console.ReadLine();
-
-        foreach (var item in inputword)
+        for (int i = 0; i < L; i++)
         {
-            hash = item - 'a'+1;
-            result += hash * Math.Pow(r, pow)%M;
-            pow++;
+            int charValue = input[i] - 'a' + 1; // 'a'는 1, 'b'는 2, ..., 'z'는 26
+            hashValue = (hashValue + charValue * pow) % M;
+            pow = (pow * r) % M; // r의 다음 거듭제곱 값을 계산
         }
 
-        Console.WriteLine(result);
-        Console.ReadLine();
+        // 결과 출력
+        Console.WriteLine(hashValue);
     }
 }
