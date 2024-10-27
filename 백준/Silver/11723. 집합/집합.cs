9a7af499dab32c3cf2fdcf7c57a2e2abc4c4e88f
levell1 <96651722+levell1@users.noreply.github.com>
2024-10-28 04:41:28 +0900

    [Silver V] Title: 집합, Time: 1464 ms, Memory: 43212 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cs
+++ 백준/Silver/11723. 집합/집합.cs
@@ -9,94 +9,46 @@ class Program
     static void Main()
     {
         List<int> list = new List<int>();
-        int[] all = new int[20] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         
         int M = Convert.ToInt32( Console.ReadLine());
+        int bitMask = 0;
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < M; i++)
         {
             string[] input = Console.ReadLine().Split(' ');
             string comment = input[0];
-            int x=0;
-            if (input.Length>1)
-            {
-                x = int.Parse(input[1]);
-            }
-            
+
+            int x = input.Length > 1 ? int.Parse(input[1]) : 0;
+
             switch (comment) 
             {
                 case "add":
-                    Add(x); 
+                    bitMask |= 1 << (x - 1);
                     break;
                 case "remove":
-                    Remove(x);
+                    bitMask &= ~(1 << (x - 1));
                     break;
                 case "check":
-                    Check(x);
+                    stringBuilder.AppendLine((bitMask & (1 << (x - 1))) != 0 ? "1":"0");
                     break;
                 case "toggle":
-                    Toggle(x);
+                    bitMask ^= 1 << (x - 1);
                     break;
                 case "all":
-                    All();
+                    bitMask = (1 << 20) - 1;
                     break;
                 case "empty":
-                    Empty();
+                    bitMask = 0;
                     break;
                 default: break;
             }
-
         }
+        
         Console.Clear();
         Console.Write(stringBuilder.ToString());
         Console.ReadLine();
 
-        void Add(int x) 
-        {
-            if (list.Contains(x))
-            {
-                return;
-            }
-            list.Add(x);
-        }
-        void Remove(int x)
-        {
-            if (list.Contains(x))
-            {
-                list.Remove(x);
-            }
-            
-        }
-        void Check(int x)
-        {
-            if (list.Contains(x))
-            {
-                stringBuilder.AppendLine("1");
-            }
-            else
-            {
-                stringBuilder.AppendLine("0");
-            }
-        }
-        void Toggle(int x)
-        {
-            if (list.Contains(x))
-            {
-                list.Remove(x);
-            }
-            else
-            {
-                list.Add(x);
-            }
-        }
-        void All()
-        {
-            list = all.ToList();
-        }
-        void Empty()
-        {
-            list.Clear();
-        }
+        
     }
 }
