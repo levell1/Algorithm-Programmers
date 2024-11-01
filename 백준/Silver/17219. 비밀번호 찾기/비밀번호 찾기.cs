2372fd4406bcc6ace0c8e0c6e9d7f921ac55e4be
levell1 <96651722+levell1@users.noreply.github.com>
2024-11-01 17:55:45 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 3008 ms, Memory: 32224 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(' '),Convert.ToInt32);
+
+        Dictionary<string,string> password = new Dictionary<string,string>();
+
+        for (int i = 0; i < input[0]; i++)
+        {
+            string[] sitePass = Console.ReadLine().Split(' ');
+            password.Add(sitePass[0], sitePass[1]);
+        }
+
+        for (int i = 0; i < input[1]; i++)
+        {
+            string find = Console.ReadLine();
+            if (password.ContainsKey(find))
+            {
+                Console.WriteLine(password[find]);
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
+
