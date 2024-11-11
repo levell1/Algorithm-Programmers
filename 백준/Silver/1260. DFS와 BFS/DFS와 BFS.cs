6c2497810f06895c70b13ce49ba0be00d1d6d996
levell1 <96651722+levell1@users.noreply.github.com>
2024-11-11 19:36:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 8476 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
@@ -10,14 +10,25 @@ class Program
         StringBuilder sb = new StringBuilder();
         
         int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
-        int[,] graph = new int[input[0]+1,input[0]+1];
         bool[] visit = new bool[input[0]+1];
-        
+
+        List<int>[] graph = new List<int>[input[0] + 1];
+        //그래프 초기화 필요.
+        for (int i = 1; i <= input[0]; i++)
+        {
+            graph[i] = new List<int>();
+        }
+
         for (int i = 0; i < input[1]; i++)
         {
             int[] line = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
-            graph[line[0], line[1]] = 1;
-            graph[line[1], line[0]] = 1;
+            graph[line[0]].Add(line[1]);
+            graph[line[1]].Add(line[0]);
+        }
+        //정렬 
+        for (int i = 1; i < graph.Length; i++)
+        {
+            graph[i].Sort();
         }
 
         DFS(input[2]);
@@ -33,11 +44,11 @@ class Program
         {
             sb.Append($"{a} ");
             visit[a] = true;
-            for (int i = 0; i <= input[0]; i++)
+            foreach (var next in graph[a])
             {
-                if (graph[a, i] == 1 && !visit[i])
+                if (!visit[next])
                 {
-                    DFS(i);
+                    DFS(next);
                 }
             }
         }
@@ -52,18 +63,16 @@ class Program
             while (queue.Count > 0)
             {
                 int current = queue.Dequeue();
-
-                for (int i = 0; i <= input[0]; i++)
+                foreach (var next in graph[current])
                 {
-                    if (graph[current, i] == 1 && !visit[i])
+                    if (!visit[next])
                     {
-                        visit[i] = true;
-                        queue.Enqueue(i);
-                        sb.Append($"{i} ");
+                        visit[next] = true;
+                        queue.Enqueue(next);
+                        sb.Append($"{next} ");
                     }
                 }
             }
         }
-
     }
 }
