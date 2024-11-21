557c504b7d867cb9d67a61bd3d39042925bafc6e
levell1 <96651722+levell1@users.noreply.github.com>
2024-11-22 08:37:35 +0900

    [Gold V] Title: 두 용액, Time: 88 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cs
+++ 백준/Gold/2470. 두 용액/두 용액.cs
@@ -19,19 +19,7 @@ class Program
         int leftNum = nums[left];
         int rightNum = nums[right];
 
-        if (nums[input - 1] < 0)
-        {
-            leftNum = nums[input - 2];
-            rightNum = nums[input - 1];
-            left = right;
-        }
-        else if (nums[0] > 0)
-        {
-            
-            leftNum = nums[0];
-            rightNum = nums[1];
-            left = right;
-        }
+
 
         while (left<right) {
 
