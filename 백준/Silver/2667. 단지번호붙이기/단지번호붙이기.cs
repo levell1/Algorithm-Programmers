3fc0ef99ee0524c8f64fd37d85d97798bf418574
levell1 <96651722+levell1@users.noreply.github.com>
2024-11-26 18:02:14 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 6212 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cs
@@ -0,0 +1,89 @@
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+
+        int input = Convert.ToInt32(Console.ReadLine());
+        int size = input;
+
+        int[,] graph = new int[size+1, size+1];
+
+        int[] movex = { 0, 0, -1, 1 }; //상 하 좌 우
+        int[] movey = { 1, -1, 0, 0 };
+
+        for (int i = 1; i <= size; i++)
+        {
+            string line =Console.ReadLine();
+
+            for (int j = 1; j <= line.Length; j++)
+            {
+                graph[i, j] = line[j - 1] -'0';
+            }
+        }
+
+        int count = 2;
+        Dictionary<int,int> houseNum = new Dictionary<int,int>();
+
+        for (int i = 1; i <= size; i++)
+        {
+
+            for (int j = 1; j <= size; j++)
+            {
+                if (graph[i, j] == 1)
+                {
+                    bfs(i,j);
+                }
+                
+            }   
+        }
+
+
+        void bfs(int goalx, int goaly)
+        {
+            Queue<(int x, int y)> q = new Queue<(int x, int y)>();
+
+            q.Enqueue((goalx, goaly));
+            graph[goalx, goaly] = count;
+            houseNum.Add(count, 1);
+            while (q.Count > 0)
+            {
+                (int curx, int cury) = q.Dequeue();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextx = curx + movex[i];
+                    int nexty = cury + movey[i];
+                    if (nextx > 0 && nextx <= size && nexty > 0 && nexty <= size)
+                    {
+                        if (graph[nextx, nexty] == 1)
+                        {
+                            q.Enqueue((nextx, nexty));
+                            graph[nextx, nexty] = count;
+                            houseNum[count]++;
+                        }
+                    }
+                }
+            }
+            count++;
+        }
+
+        sw.WriteLine($"{count-2}");
+        List<int> list = new List<int>();
+        foreach (var item in houseNum)
+        {
+            list.Add(item.Value);
+        }
+        list.Sort();
+        foreach (var item in list)
+        {
+            sw.WriteLine(item.ToString());
+        }
+
+        sw.Flush(); sw.Close();
+        Console.ReadLine();
+    }
+}
