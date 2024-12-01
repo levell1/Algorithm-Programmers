6bf6ba4d41bb911ac280b0647b0ecd14b99debb0
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-01 18:51:35 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 10068 ms, Memory: 200048 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int T = int.Parse(Console.ReadLine());
+        var sw = new System.IO.StreamWriter(Console.OpenStandardOutput());
+
+        while (T-- > 0)
+        {
+            int k = int.Parse(Console.ReadLine());
+            var maxHeap = new SortedDictionary<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+            var minHeap = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                var input = Console.ReadLine().Split();
+                char op = input[0][0];
+                int num = int.Parse(input[1]);
+
+                if (op == 'I')
+                {
+                    AddToHeap(maxHeap, num);
+                    AddToHeap(minHeap, num);
+                }
+                else if (op == 'D')
+                {
+                    if (num == 1) RemoveFromHeap(maxHeap, minHeap);
+                    else RemoveFromHeap(minHeap, maxHeap);
+                }
+            }
+
+            // 결과 출력
+            if (maxHeap.Count == 0) sw.WriteLine("EMPTY");
+            else sw.WriteLine($"{GetTop(maxHeap)} {GetTop(minHeap)}");
+        }
+
+        sw.Flush();
+        sw.Close();
+    }
+
+    static void AddToHeap(SortedDictionary<int, int> heap, int num)
+    {
+        if (!heap.ContainsKey(num)) heap[num] = 0;
+        heap[num]++;
+    }
+
+    static void RemoveFromHeap(SortedDictionary<int, int> primaryHeap, SortedDictionary<int, int> secondaryHeap)
+    {
+        if (primaryHeap.Count == 0) return;
+
+        int top = GetTop(primaryHeap);
+        primaryHeap[top]--;
+
+        if (primaryHeap[top] == 0) primaryHeap.Remove(top);
+
+        if (secondaryHeap.ContainsKey(top))
+        {
+            secondaryHeap[top]--;
+            if (secondaryHeap[top] == 0) secondaryHeap.Remove(top);
+        }
+    }
+
+    static int GetTop(SortedDictionary<int, int> heap)
+    {
+        foreach (var kvp in heap)
+        {
+            if (kvp.Value > 0) return kvp.Key;
+        }
+        return 0;
+    }
+}
