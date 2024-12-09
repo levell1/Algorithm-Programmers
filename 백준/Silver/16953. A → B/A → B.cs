75bb0ef8d69ed06c24d0aa24656194404d0026d5
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-09 12:36:06 +0900

    [Silver II] Title: A → B, Time: 52 ms, Memory: 6140 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cs
+++ 백준/Silver/16953. A → B/A → B.cs
@@ -10,62 +10,38 @@ class Program
         int start = inputArr[0];
         int end = inputArr[1];
 
-        bool[] visited = new bool[end + 1];
-
-        int min = int.MaxValue;
-
-        bfs(start);
-        if (min == int.MaxValue)
-        {
-            sw.Write(-1);
-        }
-        else
-        {
-            sw.Write(min);
-        }
-        
-
+        sw.Write(bfs(start));
         sw.Flush(); sw.Close();
         Console.ReadLine();
       
-        void bfs(int start)
+        int bfs(int start)
         {
-            visited[start] = true;
             Queue<(long num,int count)> queue = new Queue<(long, int)>();
             queue.Enqueue((start,1));
             
 
             while (queue.Count > 0)
             {
-
                 (long curnum, int curcount)   = queue.Dequeue();
                 
                 if (curnum == end)
                 {
-                    min = curcount;
-                    return;
+                    return curcount;
                 }
                 long case1 = curnum * 2;
                 long case2 = curnum * 10 + 1;
 
                 if (case1 <= end)
                 {
-                    if (!visited[case1])
-                    {
-                        visited[case1] = true;
-                        queue.Enqueue((case1, curcount + 1));
-                    }
+                    queue.Enqueue((case1, curcount + 1));
                 }
 
                 if (case2 <= end)
                 {
-                    if (!visited[case2])
-                    {
-                        visited[case2] = true;
-                        queue.Enqueue((case2, curcount + 1));
-                    }
+                    queue.Enqueue((case2, curcount + 1));
                 }
             }
+            return -1;
         }
     }
 }
