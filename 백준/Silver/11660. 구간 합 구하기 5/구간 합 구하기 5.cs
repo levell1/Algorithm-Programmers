3675440290e28e69d4d4b7b1411f1a54e6b9742f
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-11 09:53:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 264 ms, Memory: 23528 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cs
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cs
@@ -10,7 +10,6 @@ class Program
         int N = inputArr[0];
         int T = inputArr[1];
 
-        int[,] board= new int[N+1,N+1];
         int[,] dp = new int[N+1,N+1];
 
         for (int i = 1; i <= N; i++)
@@ -18,8 +17,7 @@ class Program
             int[] nums = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
             for (int j = 1; j <= N; j++)
             {
-                board[i, j] = nums[j-1];
-                dp[i, j] = dp[i - 1, j] + dp[i, j - 1] - dp[i - 1, j - 1] + board[i, j];
+                dp[i, j] = dp[i - 1, j] + dp[i, j - 1] - dp[i - 1, j - 1] + nums[j - 1];
             }
         }
 
@@ -35,7 +33,6 @@ class Program
             int result = dp[endX, endY] - dp[startX - 1, endY] - dp[endX, startY-1]+ dp[startX - 1, startY - 1];
             sw.WriteLine(result);
         }
-
 
         sw.Flush(); sw.Close();
         Console.ReadLine();
