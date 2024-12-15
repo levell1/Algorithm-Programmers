e31c40c33fe64992ab19c6217da8449962d10004
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-15 15:37:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 84 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cs
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cs
@@ -1,70 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int N = input[0];
+        int K = input[1];
+
+        int max = 100000; 
+        int[] dist = new int[max + 1];
+        Array.Fill(dist, int.MaxValue);
+        dist[N] = 0;
+
+        Console.WriteLine(Dijkstra(N, K, dist));
+    }
+
+    static int Dijkstra(int start, int end, int[] dist)
+    {
+        var pq = new PriorityQueue<(int position, int time), int>();
+        pq.Enqueue((start, 0), 0);
+
+        while (pq.Count > 0)
         {
-            var sw = new StreamWriter(Console.OpenStandardOutput());
+            var (current, currentTime) = pq.Dequeue();
 
-            //int T = Convert.ToInt32(Console.ReadLine());
-            int[] inputArr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
-            int N = inputArr[0];
-            int K = inputArr[1];
+            if (currentTime > dist[current]) continue;
 
-            int max = 100000;
+            // 3가지 이동 방식 처리
+            int[] nextPositions = { current - 1, current + 1, current * 2 };
+            int[] costs = { 1, 1, 0 };
 
-            var graph = new List<(int, int)>[max + 1];
-
-            graph[0] = new List<(int, int)>();
-            graph[0].Add((1, 1));
-            graph[max] = new List<(int, int)>();
-            graph[max].Add((max - 1, 1));
-
-            for (int i = 1; i < max; i++)
+            for (int i = 0; i < 3; i++)
             {
-                graph[i] = new List<(int, int)>();
-                graph[i].Add((i - 1, 1));
-                graph[i].Add((i + 1, 1));
-                if (i*2<= max)
+                int next = nextPositions[i];
+                int nextCost = currentTime + costs[i];
+
+                if (next >= 0 && next <= 100000 && nextCost < dist[next])
                 {
-                    graph[i].Add((i * 2, 0));
+                    dist[next] = nextCost;
+                    pq.Enqueue((next, nextCost), nextCost);
                 }
-            }
-
-            int[] dist = new int[max+1];
-            Array.Fill(dist, int.MaxValue);
-            dist[N] = 0;
-
-            sw.WriteLine($"{Dijkstra(dist, graph, N, K)}");
-            sw.Flush(); sw.Close();
-
-
-            int Dijkstra(int[] dist, List<(int, int)>[] graph, int start, int end)
-            {
-
-                var pq = new PriorityQueue<(int, int), int>();          
-                pq.Enqueue((start, 0), 0);
-                
-                while (pq.Count > 0)
-                {
-                    var (current, currentCost) = pq.Dequeue();
-                    if (currentCost > dist[current]) continue;         
-
-                    foreach (var (next, cost) in graph[current])       
-                    {
-                        int nextCost = currentCost + cost;
-                        if (nextCost < dist[next])                     
-                        {
-                            dist[next] = nextCost;                     
-                            pq.Enqueue((next, nextCost), nextCost);    
-                        }
-                    }
-                }
-
-                return dist[end];
             }
         }
+
+        return dist[end];
     }
 }
