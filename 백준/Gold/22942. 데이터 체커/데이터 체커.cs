31aa3b07acc82570b1e8d65cce2c4ae45dac2340
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-27 19:13:02 +0900

    [Gold IV] Title: 데이터 체커, Time: 2232 ms, Memory: 22440 KB -BaekjoonHub

--- 백준/Gold/22942. 데이터 체커/데이터 체커.cs
+++ 백준/Gold/22942. 데이터 체커/데이터 체커.cs
@@ -2,62 +2,60 @@ class Program
 {
     static void Main()
     {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        int input = Convert.ToInt32(Console.ReadLine());
+        //int[] inputArr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+
+        var circles = new List<(int x, int r)>();
+
+        string result = "YES";
+        for (int i = 0; i < input; i++)
         {
-            var sw = new StreamWriter(Console.OpenStandardOutput());
+            int[] circle = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+            int x = circle[0];
+            int r = circle[1];
+            circles.Add((x, r));
 
-            int input = Convert.ToInt32(Console.ReadLine());
-            //int[] inputArr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        }
+        circles.Sort((a, b) => (b.x - b.r).CompareTo(a.x - a.r));
 
-            var circles = new List<(int x, int r)>();
 
-            string result = "YES";
-            for (int i = 0; i < input; i++)
+        for (int i = circles.Count - 1; i >= 0; i--)
+        {
+            int x = circles[i].x;
+            int r = circles[i].r;
+            int start = x - r;
+            for (global::System.Int32 j = circles.Count - 1; j > i; j--)
             {
-                int[] circle = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
-                int x = circle[0];
-                int r = circle[1];
-                circles.Add((x, r));
-
-            }
-            circles.Sort((a, b) => (b.x - b.r).CompareTo(a.x - a.r));
-
-
-            for (int i = circles.Count - 1; i >= 0; i--)
-            {
-                int x = circles[i].x;
-                int r = circles[i].r;
-                int start = x - r;
-                for (global::System.Int32 j = circles.Count - 1; j > i; j--)
+                var (x1, r1) = circles[j];
+                int d = Math.Abs(x - x1);
+                int re = Math.Abs(r - r1);
+                if (r + r1 < d || d < re)
                 {
-                    var (x1, r1) = circles[j];
-                    int d = Math.Abs(x - x1);
-                    int re = Math.Abs(r - r1);
-                    if (r + r1 < d || d < re)
-                    {
-                        result = "YES";
-                    }
-                    else
-                    {
-                        result = "NO";
-                        break;
-                    }
-
-                    if (x1 + r1 < start)
-                    {
-                        circles.RemoveAt(j);
-                    }
+                    result = "YES";
                 }
-
-                if (result == "NO")
+                else
                 {
+                    result = "NO";
                     break;
                 }
 
+                if (x1 + r1 < start)
+                {
+                    circles.RemoveAt(j);
+                }
             }
 
-            sw.WriteLine(result);
-            sw.Flush(); sw.Close();
+            if (result == "NO")
+            {
+                break;
+            }
+
         }
+
+        sw.WriteLine(result);
+        sw.Flush(); sw.Close();
 
     }
 }
