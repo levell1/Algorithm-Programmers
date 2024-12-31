c48dd9b7964579a685b871b7b402ec7be7bb2cb8
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-31 13:59:22 +0900

    [Silver IV] Title: 대칭 차집합, Time: 184 ms, Memory: 41628 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cs
@@ -0,0 +1,49 @@
+
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        //int input = Convert.ToInt32(Console.ReadLine());
+        int[] inputArr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        int ab = inputArr[0];
+        int ba = inputArr[1];
+
+        var nums1 =new HashSet<int>();
+        var nums2 = new HashSet<int>();
+
+        int[] first = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        for (int i = 0; i < inputArr[0]; i++)
+        {
+            nums1.Add(first[i]);
+        }
+
+        int[] second = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        for (int i = 0; i < inputArr[1]; i++)
+        {
+            if (nums1.Contains(second[i])) 
+            {
+                ab--;
+            }
+            nums2.Add(second[i]);
+        }
+
+        for (int i = 0; i < inputArr[0]; i++)
+        {
+            if (nums2.Contains(first[i]))
+            {
+                ba--;
+            }
+        }
+
+
+        Console.WriteLine($"{ab+ba}");
+        
+        sw.Flush(); sw.Close();
+
+    }
+}
