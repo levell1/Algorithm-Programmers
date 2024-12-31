36c1990bf1dd92cddcb6bcd2347ddd9132417048
levell1 <96651722+levell1@users.noreply.github.com>
2024-12-31 14:40:21 +0900

    [Silver II] Title: 싸이버개강총회, Time: 172 ms, Memory: 28552 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cs
@@ -0,0 +1,66 @@
+
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        //int input = Convert.ToInt32(Console.ReadLine());
+        //[] inputArr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        
+        string[] inputArr = Console.ReadLine().Split(" ");
+        int[] first = Array.ConvertAll(inputArr[0].Split(":"), Convert.ToInt32);
+        int[] start = Array.ConvertAll(inputArr[1].Split(":"), Convert.ToInt32);
+        int[] end = Array.ConvertAll(inputArr[2].Split(":"), Convert.ToInt32);
+        int firstTime = first[0] * 60 + first[1];
+        int startTime = start[0] * 60 + start[1];
+        int endTime = end[0] * 60 + end[1];
+
+        bool check = false;
+        if (endTime<startTime)
+        {
+            endTime += (24 * 60 - startTime);
+            check = true;
+        }
+
+        string input;
+
+        var attendance = new HashSet<string>();
+        var attendanceCheck = new HashSet<string>();
+
+        while ((input=Console.ReadLine())!=null) 
+        {
+            if (input=="")
+            {
+                break;
+            }
+            string[] info = input.Split(" ");
+            int[] time = Array.ConvertAll(info[0].Split(":"), Convert.ToInt32);
+            int currentTime = time[0] * 60 + time[1];
+
+            if (check && currentTime < firstTime)
+            {
+                endTime += (24 * 60 - startTime);
+            }
+
+            if (currentTime<=firstTime)
+            {
+                attendance.Add(info[1]);
+            }else if (currentTime >= startTime && currentTime <= endTime)
+            {
+                if (attendance.Contains(info[1]))
+                {
+                    attendanceCheck.Add(info[1]);
+                }
+            }
+        }
+        
+        sw.Write(attendanceCheck.Count);
+
+        sw.Flush(); sw.Close();
+
+    }
+}
