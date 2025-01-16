168d1aa7e9b0334c28c13b17ce1781e5b7a995dd
levell1 <96651722+levell1@users.noreply.github.com>
2025-01-16 14:36:48 +0900

    [Bronze I] Title: 소인수분해, Time: 56 ms, Memory: 5616 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cs
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cs
@@ -11,17 +11,19 @@ class Program
         int input = Convert.ToInt32(Console.ReadLine());
 
         int cur = input;
-        while (cur!=1)
+
+        for (int i = 2; i * i <= cur; i++) 
         {
-            for (global::System.Int32 i = 2; i <= cur; i++)
+            while (cur % i == 0) 
             {
-                if (cur%i==0)
-                {
-                    sw.WriteLine(i);
-                    cur /= i;
-                    i = 1;
-                }
+                sw.WriteLine(i);
+                cur /= i; 
             }
+        }
+
+        if (cur > 1) 
+        {
+            sw.WriteLine(cur);
         }
 
         sw.Flush(); sw.Close();
