a3df6f40760f0f79a7682db993d3fa0a344fcf2d
levell1 <96651722+levell1@users.noreply.github.com>
2025-07-21 23:02:32 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 160 ms, Memory: 21644 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cs
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cs
@@ -1,7 +1,4 @@
 
-using System.Text;
-using System.Xml.Linq;
-
 class Program
 {
     static void Main()
@@ -10,45 +7,30 @@ class Program
             var sw = new StreamWriter(Console.OpenStandardOutput());
 
             int[] inputarr = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+            int n = inputarr[0];
+            int e = inputarr[1];
 
-            int node = inputarr[0];
-            int link = inputarr[1];
+            var graph = new List<(int, int)>[n + 1];  // 그래프 인접리스트
 
-            var graph = new List<(int, int)>[node + 1];  // 그래프 인접리스트
-
-            for (int i = 1; i <= node; i++)
+            for (int i = 1; i <= n; i++)
             {
                 graph[i] = new List<(int, int)>();
             }
 
-            for (int i = 0; i < link; i++)
+            for (int i = 0; i < e; i++)
             {
-                int[] info = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
-                int curStart = info[0];
-                int curEnd = info[1];
-                int cost = info[2];
+                int[] edge = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
 
-                graph[curStart].Add((curEnd, cost));
-                graph[curEnd].Add((curStart, cost));
+                graph[edge[0]].Add((edge[1], edge[2]));
+                graph[edge[1]].Add((edge[0], edge[2]));        //양방향
             }
-
-         
 
             int[] go = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
             int firstPoint = go[0];
             int secondPoint = go[1];
 
-            long tofirst = Dijkstra(graph, node, 1, firstPoint);
-            long firstToSecond = Dijkstra(graph, node, firstPoint, secondPoint);
-            long SecondtoLast = Dijkstra(graph, node, secondPoint, node);
-
-            long path1 = tofirst + firstToSecond + SecondtoLast;
-
-            long toSecond = Dijkstra(graph, node, 1, secondPoint);
-            long SecondtoFirst= Dijkstra(graph, node, secondPoint, firstPoint);
-            long FirsttoLast2 = Dijkstra(graph, node, firstPoint, node);
-
-            long path2 = toSecond + SecondtoFirst + FirsttoLast2;
+            long path1 = SumPathDist(graph, n, 1, firstPoint, secondPoint, n);
+            long path2 = SumPathDist(graph, n, 1, secondPoint, firstPoint, n);
 
             long result = Math.Min(path1, path2);
 
@@ -64,6 +46,17 @@ class Program
 
     }
 
+    static long SumPathDist(List<(int, int)>[] graph, int n, int start,int point1,int point2, int end) 
+    {
+        long d1 = Dijkstra(graph, n, 1, point1);
+        long d2 = Dijkstra(graph, n, point1, point2);
+        long d3 = Dijkstra(graph, n, point2, n);
+
+        long dist = d1+d2+d3;
+
+        return dist;
+    }
+
 
     static long Dijkstra(List<(int, int)>[] graph,int n, int start, int end)
     {
@@ -71,7 +64,7 @@ class Program
         Array.Fill(dist, int.MaxValue);
         dist[start] = 0;
 
-        var pq = new PriorityQueue<(int, int), int>();          //우선순위 큐
+        var pq = new PriorityQueue<(int node, long cost), long>();          //우선순위 큐
         pq.Enqueue((start, 0), 0);
 
         while (pq.Count > 0)
@@ -86,7 +79,7 @@ class Program
                 if (nextCost < dist[next])                      // 현재 start~next까지의 비용이 원래값보다 작으면 
                 {
                     dist[next] = nextCost;                      // 갱신
-                    pq.Enqueue((next, (int)nextCost), (int)nextCost);     // pq에 추가, next와 연결된 다음경로 탐색
+                    pq.Enqueue((next, nextCost), nextCost);     // pq에 추가, next와 연결된 다음경로 탐색
                 }
             }
         }
