a421e3b78180b96efc1f6558bc77d5541f41640d
levell1 <96651722+levell1@users.noreply.github.com>
2025-07-26 18:04:01 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cs
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cs
@@ -1,52 +1,47 @@
 
 
+using System.Diagnostics.CodeAnalysis;
+
 class Program
 {
+    static bool[] check = new bool[10001];
+
     static void Main()
     {
 
         var sw = new StreamWriter(Console.OpenStandardOutput());
 
-        List<int> list = new List<int>();
-
-        int num = 0;
-        int curNum = 1;
-        int nextNum = 1;
-
-        for (int i = 1; i < 10000; i++)
-        {
-            list.Add(i);
-        }
 
         for (int i = 1; i <= 10000; i++)
         {
-            list.Remove(nextNum);
-
-            curNum = i;
-
-            nextNum = curNum;
-            nextNum += curNum / 10000;
-            curNum %= 10000;
-            nextNum += curNum / 1000;
-            curNum %= 1000;
-            nextNum += curNum / 100;
-            curNum %= 100;
-            nextNum += curNum / 10;
-            curNum %= 10;
-            nextNum += curNum;
+            int result = d(i);
+            if (result<=10000)
+            {
+                check[result] = true;
+            }
         }
 
-        list.Add(1);
-
-        list.Sort();
-
-        foreach (var item in list)
+        for (int i = 1; i < 10000; i++)
         {
-            sw.WriteLine(item);   
+            if (!check[i])
+            {
+                sw.WriteLine(i);
+            }
         }
 
         sw.Flush(); sw.Close();
 
+    }
+
+    static int d(int n) 
+    {
+        int sum = n;
+        while (n>=1)
+        {
+            sum += n% 10;
+            n /= 10;
+        }
+        return sum;
     }
 
 }
