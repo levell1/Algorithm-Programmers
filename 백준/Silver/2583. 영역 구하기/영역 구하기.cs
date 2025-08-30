08e362b5451ae00c391a0f6add4ef96a6868fff2
levell1 <96651722+levell1@users.noreply.github.com>
2025-08-30 11:45:08 +0900

    [Silver I] Title: 영역 구하기, Time: 56 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cs
@@ -0,0 +1,91 @@
+
+using System.ComponentModel;
+
+class Program
+{
+    static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+    static int result;
+    static int[,] field;
+    static bool[,] visited;
+    static readonly int[] dx = { 0, 0, 1, -1 };
+    static readonly int[] dy = { 1, -1, 0, 0 };
+
+    static int m ;
+    static int n ;
+    static int k ;
+
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+
+        m = input[0];
+        n = input[1];
+        k = input[2];
+
+        field = new int[n, m];
+        visited = new bool[n, m];
+
+        List<int> results = new List<int>();
+
+        for (int i = 0; i < k; i++)
+        {
+            int[] square = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+
+            for (int j = square[0]; j < square[2]; j++)
+            {
+                for (int l = square[1]; l < square[3]; l++)
+                {
+                    visited[j, l] = true;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!visited[i,j])
+                {
+                    results.Add(BFS(i, j));
+                }
+            }
+        }
+
+        results.Sort();
+        sw.WriteLine(results.Count);
+        foreach (int result in results)
+        {
+            sw.WriteLine(result);
+        }
+
+        
+        sw.Flush(); sw.Close();
+    }
+
+    static int BFS(int x, int y) 
+    {
+        Queue<(int, int)> que = new Queue<(int, int)>();
+        que.Enqueue((x, y));
+        visited[x, y] = true;
+        int extent = 1;
+        while (que.Count>0)
+        {
+            var (curx,cury) = que.Dequeue();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = curx + dx[i];
+                int ny = cury + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+                {
+                    if (!visited[nx,ny])
+                    {
+                        visited[nx, ny] = true;
+                        que.Enqueue((nx, ny));
+                        extent++;
+                    }
+                }
+            }
+        }
+        return extent;
+    }
+}
