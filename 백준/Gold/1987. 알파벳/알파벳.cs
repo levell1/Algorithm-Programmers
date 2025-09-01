98005d127faba1bcafe6b5f52e91ff79a0fff429
levell1 <96651722+levell1@users.noreply.github.com>
2025-09-01 14:24:21 +0900

    [Gold IV] Title: 알파벳, Time: 1304 ms, Memory: 5596 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cs
@@ -0,0 +1,72 @@
+
+
+class Program
+{
+    static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+    static char[,] board;
+    static bool[,] visited;
+    static int[] alphabet;
+
+    static readonly int[] dx = { 0, 0, 1, -1 };
+    static readonly int[] dy = { 1, -1, 0, 0 };
+
+    static int r ;
+    static int c ;
+    static int max;
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+
+        r = input[0];
+        c = input[1];
+
+        board = new char[r, c];
+        visited = new bool[r, c];
+        alphabet = new int[26];
+
+        max = 0;
+
+        for (int i = 0; i < r; i++)
+        {
+            string str = Console.ReadLine();
+
+            for (int j = 0; j < c; j++)
+            {
+                board[i, j] = str[j];
+            }
+        }
+
+        DFS(0, 0,1);
+
+        sw.WriteLine(max);
+        sw.Flush(); sw.Close();
+    }
+    
+    static void DFS(int x, int y,int count) 
+    {
+        visited[x, y] = true;
+        alphabet[board[x, y] - 'A']++;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx >= 0 && ny >= 0 && nx < r && ny < c)
+            {
+                if (alphabet[board[nx, ny] - 'A'] == 0)
+                {
+                    DFS(nx, ny, count + 1);
+                    visited[nx, ny] = false;
+                    alphabet[board[nx, ny] - 'A'] = 0;
+                }
+            }
+        }
+        if (max<count)
+        {
+            max = count;
+        }
+
+    }
+}
